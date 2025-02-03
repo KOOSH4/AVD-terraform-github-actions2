28c5a7f9576c8c24d633e143612d7fadc19355c9
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-02-03 22:18:40 +0100

    Fix host pool ID reference in Azure Virtual Desktop registration resource

--- main.tf
+++ main.tf
@@ -149,6 +149,6 @@ resource "azurerm_virtual_machine_extension" "aad_login" {
   depends_on                 = [azurerm_windows_virtual_machine.vm]
 }
 resource "azurerm_virtual_desktop_host_pool_registration_info" "registration" {
-  hostpool_id     = azurerm_virtual_desktop_host_pool.avd_host_pool.id
+  hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
   expiration_date = timeadd(timestamp(), "48h") # Extended token validity
 }
