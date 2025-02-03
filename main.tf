0d64ddcfea9ea2de5f29f6508c0f1ca13ab643b4
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-02-03 22:57:30 +0100

    Add AVD agent installation script using custom script extension in Terraform configuration

--- main.tf
+++ main.tf
@@ -155,7 +155,7 @@ resource "azurerm_virtual_machine_extension" "aad_login" {
   depends_on                 = [azurerm_windows_virtual_machine.vm]
 }
 
-
+# Add a custom script extension to install the AVD agent and register the VM
 resource "azurerm_virtual_machine_extension" "avd_agent" {
   name                       = "AVDAgent"
   virtual_machine_id         = azurerm_windows_virtual_machine.vm.id
@@ -164,26 +164,18 @@ resource "azurerm_virtual_machine_extension" "avd_agent" {
   type_handler_version       = "1.10"
   auto_upgrade_minor_version = true
 
-  settings = jsonencode({
-    commandToExecute = <<-EOT
-      # Install AVD Agent
-      $RegInfo = @{
-        Token = "${azurerm_virtual_desktop_host_pool_registration_info.registration.token}"
-        RegisteredResourceGroup = "${azurerm_resource_group.rg-AVD2.name}"
-        RegisteredSubscriptionId = var.subscription_id
-      }
-      New-Item -Path "C:\\Temp" -ItemType Directory -Force
-      Invoke-WebRequest -Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4wEav" -OutFile "C:\\Temp\\AVDAgent.msi"
-      Start-Process msiexec.exe -Wait -ArgumentList "/i C:\\Temp\\AVDAgent.msi /quiet /qn /norestart ADDLOCAL=ALL /l*v C:\\Temp\\AVDAgentInstall.log REGISTRATIONTOKEN=$($RegInfo.Token)"
-      
-      # Install AVD Boot Loader
-      Invoke-WebRequest -Uri "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4wqLj" -OutFile "C:\\Temp\\AVDBootLoader.msi"
-      Start-Process msiexec.exe -Wait -ArgumentList "/i C:\\Temp\\AVDBootLoader.msi /quiet /qn /norestart /l*v C:\\Temp\\AVDBootLoaderInstall.log"
-    EOT
-  })
+  settings = <<SETTINGS
+    {
+      "fileUris": ["https://raw.githubusercontent.com/Azure/RDS-Templates/master/wvd-templates/avd-agent-installer.ps1"],
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File avd-agent-installer.ps1 -HostPoolARMPath ${azurerm_virtual_desktop_host_pool.hostpool.id} -AadTenantId ${data.azurerm_client_config.current.tenant_id} -RegistrationToken '${azurerm_virtual_desktop_host_pool_registration_info.registration.token}'"
+    }
+SETTINGS
 
   depends_on = [
-    azurerm_virtual_desktop_host_pool_registration_info.registration,
-    azurerm_windows_virtual_machine.vm
+    azurerm_virtual_machine_extension.aad_login,
+    azurerm_virtual_desktop_host_pool_registration_info.registration
   ]
 }
+
+# Retrieve the tenant ID for the current subscription
+data "azurerm_client_config" "current" {}
