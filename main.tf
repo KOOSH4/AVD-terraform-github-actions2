2ac2f82f54eebdc766964c3dce184194aa3d09d6
KOOSHA <81762308+KOOSH4@users.noreply.github.com>
2025-02-22 21:53:17 +0100

    Remove obsolete DSC extension configuration and add application group association for virtual desktop workspace

--- main.tf
+++ main.tf
@@ -150,36 +150,11 @@ resource "azurerm_virtual_machine_extension" "aad_login" {
   depends_on                 = [azurerm_windows_virtual_machine.vm]
 }
 
-resource "azurerm_virtual_machine_extension" "avd_dsc" {
-  name                       = "vm0-avd-dsc"
-  virtual_machine_id         = azurerm_windows_virtual_machine.vm.id
-  publisher                  = "Microsoft.Powershell"
-  type                       = "DSC"
-  type_handler_version       = "2.73"
-  auto_upgrade_minor_version = true
 
-  settings = <<SETTINGS
-  {
-    "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_09-08-2022.zip",
-    "configurationFunction": "Configuration.ps1\\AddSessionHost",
-    "properties": {
-      "HostPoolName": "${azurerm_virtual_desktop_host_pool.hostpool.name}"
-    }
-  }
-SETTINGS
-
-  protected_settings = <<PROTECTED_SETTINGS
-  {
-    "properties": {
-      "registrationInfoToken": "${azurerm_virtual_desktop_host_pool_registration_info.registration.token}"
-    }
-  }
-PROTECTED_SETTINGS
-
-  depends_on = [azurerm_virtual_desktop_host_pool.hostpool]
+resource "azurerm_virtual_desktop_workspace_application_group_association" "dag_workspace_assoc" {
+  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+  application_group_id = azurerm_virtual_desktop_application_group.dag.id
 }
-
-
 
 
 resource "azurerm_log_analytics_workspace" "avd_logs" {
@@ -245,10 +220,7 @@ resource "azurerm_monitor_metric_alert" "avd_cpu_alert" {
 
 }
 
-resource "azurerm_virtual_desktop_workspace_application_group_association" "dag_workspace_assoc" {
-  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
-  application_group_id = azurerm_virtual_desktop_application_group.dag.id
-}
+
 #### FSLOGIX ####
 
 resource "azurerm_storage_account" "fslogix_storage" {
